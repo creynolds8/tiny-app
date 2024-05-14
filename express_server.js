082c5710a058bb5e1ea1e1b8506b05caabe14029
creynolds8 <cameron8.cr@gmail.com>
2024-05-13 17:24:34 -0700

    add correct call to get longURL from urlDatabase

--- express_server.js
+++ express_server.js
@@ -27,7 +27,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars= { id: req.params.id, longURL: req.params.id.longURL };
+  const templateVars= { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
