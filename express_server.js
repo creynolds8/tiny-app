8f3e444eedbad8b675a7d15caebadfd15e73a6ec
creynolds8 <cameron8.cr@gmail.com>
2024-05-14 09:29:26 -0700

    add redirect to url show page for new url

--- express_server.js
+++ express_server.js
@@ -38,7 +38,9 @@ app.post('/urls', (req, res) => {
   let key = generateRandomString(str)
   urlDatabase[key] = req.body.longURL;
   console.log(urlDatabase);
-  res.send('ok');
+  res.redirect(`/urls/${key}`)
+    const templateVars = { id: key, longURL: urlDatabase[req.params.id] };
+    res.render('urls_show', templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -50,7 +52,7 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars= { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
