67c9222dca71725add645d61c40bed57f75c92fa
creynolds8 <cameron8.cr@gmail.com>
2024-05-15 15:47:59 -0700

    add checks for valid inputs and if given email is already registered

--- functions/functions.js
+++ functions/functions.js
@@ -11,16 +11,38 @@ const generateRandomString = function() {
 };
 
 const createUser = function(userInfo, userDatabase) {
+  // check if in put fields are valid
+  const keyArr = Object.keys(userInfo);
+  for (const key of keyArr) {  
+  if (!userInfo[key]) {
+      return { error: 'Blank field', user: null };
+    }
+  };
+  for (const user in userDatabase) {
+    if (userInfo.email === userDatabase[user].email) {
+      return { error: 'That email is already registered', user: null };
+    }
+  }
   const userId = generateRandomString();
   userDatabase[userId] = {
     id: userId,
     email: userInfo.email,
     password: userInfo.password
   };
-  return userDatabase[userId];
-}
+  return { error: null, user: userDatabase[userId] };
+};
+
+const findUser = function(userId, userDatabase) {
+  for (const user in userDatabase) {
+    if (user === userId) {
+      return { error: null, user: user};
+    }
+  }
+  return { error: 'User doesnt exist', user: null };
+};
 
 module.exports = {
   generateRandomString,
   createUser,
+  findUser,
 };
