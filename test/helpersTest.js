a4a58da50ad715f4a5d83ea56a8c57324c75f441
creynolds8 <cameron8.cr@gmail.com>
2024-05-16 21:32:52 -0700

    add generated security tests. see readme for explanation of error i ran into

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,7 @@
 const assert = require('chai').assert;
 const { findUserByEmail } = require('../helpers');
 
+
 const testUsers = {
   user1: {
     id: 'user1id',
@@ -43,5 +44,64 @@ describe('findUserByEmail', function() {
     assert.isNull(result.user)
     assert.equal(result.error, 'Incorrect Password')
   });
+});
 
+//CHATGPT generated tests
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+const agent = chai.request.agent('http://localhost:8080');
+describe('Testing URL routes', () => {
+  
+  it('GET / should redirect to /login with status 302', () => {
+    return agent
+      .get('/')
+      .then(res => {
+        expect(res).to.redirectTo('http://localhost:8080/login');
+        // expect(res).to.have.status(302); see README.md *2* for info
+      });
+  });
+
+  it('GET /urls/new should redirect to /login with status 302', () => {
+    return agent
+      .get('/urls/new')
+      .then(res => {
+        expect(res).to.redirectTo('http://localhost:8080/login');
+        // expect(res).to.have.status(302); see README.md *2* for info
+      });
+  });
+
+  it('GET /urls/NOTEXISTS should return status 404', () => {
+    return agent
+      .get('/urls/NOTEXISTS')
+      .then(res => {
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('GET /urls/b2xVn2 should return status 403', () => {
+    return agent
+      .get('/urls/b2xVn2')
+      .then(res => {
+        expect(res).to.have.status(403);
+      });
+  });
+  it('should return 403 status code for unauthorized access to "http://localhost:3000/urls/b2xVn2"', () => {
+    return agent
+      .post("/login")
+      .send({ email: "user2@example.com", password: "dishwasher-funk" })
+      .then((loginRes) => {
+        return agent.get("/urls/b2xVn2").then((accessRes) => {
+          expect(accessRes).to.have.status(403);
+        });
+      });
+  });
+
+  after(() => {
+    agent.close();
+  });
 });
